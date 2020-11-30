99809be66e7078ce56c76b6efc3192342ee7aa4e
Jantero93 <janzku1993@hotmail.com>
2020-11-30 13:49:30 +0200

    proju show far. Tarvii siistia graafista puolta. randomness korttipakan sekottaessa huono

---
+++ Kehittyneet_graafinenKorttipeli/Korttipakka.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kehittyneet_graafinenKorttipeli
+{
+    class Korttipakka
+    {
+        /* Kortit listaan kun siinä on kivoja sorttaus metodeja valmiina, pelin toteutuksessa stäkki */
+       private List<Kortti> korttiLista = new List<Kortti>();
+       private Stack<Kortti> korttiPakka = new Stack<Kortti>();
+
+        //tekee konstruktorissa valmiiksi sekoitetun stäkin
+        public Korttipakka()
+        {            
+            for (int i = 2; i < 15; i++)
+            {
+                foreach (MAA maa in Enum.GetValues(typeof(MAA)))
+                    korttiLista.Add(new Kortti(i, maa));
+            }
+
+            this.sekoitaKorttilista();
+
+            foreach (Kortti kortti in korttiLista)
+            {
+                korttiPakka.Push(kortti);
+            }
+        }
+
+        public void tulostaKokoLista()
+        {            
+            for (int i = 0; i < korttiLista.Count(); i++)
+            {
+                Console.Write(korttiLista.ElementAt(i) + "\n");
+            }   
+            
+        }        
+
+        private void jarjestaKorttilista()
+        {
+            korttiLista = korttiLista.OrderBy(p => p.arvo).ToList();
+        }
+
+        private void sekoitaKorttilista()
+        {
+            korttiLista = korttiLista.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+
+        public void sekoitaKorttiPakka()
+        {
+            List<Kortti> temp_kortit = new List<Kortti>();
+
+            while (korttiPakka.Count > 0)
+                temp_kortit.Add(korttiPakka.Pop());
+
+            temp_kortit = temp_kortit.OrderBy(a => Guid.NewGuid()).ToList();
+
+            foreach (Kortti kortti in temp_kortit)
+                korttiPakka.Push(kortti);
+
+        }
+
+        public override string ToString()
+        {
+            string tuloste = "";
+            foreach (Kortti kortti in korttiPakka)
+            {
+               tuloste += kortti + "\n";
+            }
+            return tuloste;
+        }
+
+        public Kortti annaKortti()
+        {
+            return korttiPakka.Pop();
+        }
+
+
+        public int getStackKoko()
+        {
+            return korttiPakka.Count();
+        }
+
+    }
+}
